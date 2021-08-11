9ba026429e381309c214b9bf8ec9841077131430
sagarduwal <sagarduwal0102@hotmail.com>
2021-08-11 13:19:29 +0545

    🎨 [change] Contract license to MIT

--- contracts/payout/PayoutClaimDistributor.sol
+++ contracts/payout/PayoutClaimDistributor.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.7.6 <0.8.0;
 
 import {MerkleProof} from "@openzeppelin/contracts/cryptography/MerkleProof.sol";
